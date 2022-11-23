5bd5f8e5a206b25f84cfb229ce089a16dbf77e0b
yajima <wadaluuuu@gmail.com>
2022-11-23 16:16:15 +0900

    何もわからない

---
+++ python/main.py
@@ -0,0 +1,16 @@
+class sudoku():
+    #二次元配列を書きたい
+    def array(self):
+        list2 = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+
+    #標準入力から問題を入力するメソッドを書きたい
+    def input(self):
+        pass
+
+    #標準出力から整形して出力するメソッドを書きたい
+    def print(self):
+        pass
